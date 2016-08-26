bab361dd5f961084bea85e2b01b3d7ac66ed6049
su <su.myat.hlaing@frobom.com>
2016-08-26 17:20:02 +0630

    createProject

--- sumyat/main.js
+++ sumyat/main.js
@@ -12,7 +12,9 @@ $(document).ready( function() {
 
 			$(".center").css({width:"100%"});
 
-			$(".rightOfCenter").css({width:"83.1%"});
+			$(".rightOfCenter").css({width:"83%"});
+
+			$(".rightBottomOfCenter").css({width:"82.9%"});
 		}
 
 		else{
@@ -25,7 +27,9 @@ $(document).ready( function() {
 
 			$(".center").css({width:"73%"});
 
-			$(".rightOfCenter").css({width:"76.8%"});	
+			$(".rightOfCenter").css({width:"76.7%"});	
+
+			$(".rightBottomOfCenter").css({width:"76.5%"});
 		}	
 
 	});
@@ -96,33 +100,76 @@ $(document).ready(function(){
 
 function leftAndRightCreateDiv(){
 
-	var leftOfCenter = document.createElement('div');	
+	var leftOfCenter = $('<div/>');	
 
 	$(leftOfCenter).attr({"class":"leftOfCenter"});
 
-	var rightOfCenter = document.createElement('div');
+	var rightOfCenter = $('<div/>');
 
-	$(rightOfCenter).attr({"class":"rightOfCenter"});	
+	$(rightOfCenter).attr({"class":"rightOfCenter"});
 
-	$('#centerDiv').append(leftOfCenter,rightOfCenter);
+	var rightBottomOfCenter = $('<div/>');
+
+	$(rightBottomOfCenter).attr({"class":"rightBottomOfCenter"});
+
+	$('#centerDiv').append(leftOfCenter,rightOfCenter,rightBottomOfCenter);
 
 	$('.leftOfCenter').append("<div class='titleMenu' id='titleMenuID'>"
 
-		+ "<div class='createButton'>+</div></div> <br>"
+		+ "<div class='createButton'>+</div></div>"
 
-		+ "<div class='newProjectCreateDiv'></div>");	
+		+ "<div class='newProjectCreateDiv'><ul id='menu'></ul></div>");
 
+	//$('.rightOfCenter').append("Thisisrightofcenterpage.Welcomefromthispage..Welcomefromthispage..Welcomefromthispage..Welcomefromthispage.HelloHowAreyouAreyouOKWhatareyoudoingnowWhatareyoudoingnowWhatareyoudoingnow");
+
+	//$('.rightBottomOfCenter').append("this is a asfok afoa asfj afsjf afkajfal ljafa oijfa oijfa ijafa oifjajwfaageagaegageaggawtregeagbeagae oiajfka aiwjfda aoiwjfaiowf alijwdoiawf awifjwaoif <br>ijsef<br>ekl<br>ojef<br>kf<br>kjoae<br>");
+
+	var counter=0;
+	
 	$('.createButton').click(function(){
 
-		var newTextBoxDiv = $(document.createElement('div')).attr("id", "TextBoxDiv");		
+		var newTextBoxDiv = $('<div/>').attr("id", "TextBoxDiv");		
 
-		newTextBoxDiv.after().html('<input type="text" id="textbox">');			
+		newTextBoxDiv.after().html('<form action=""><input type="text" id="textbox"></form>');			
 
 		if($('#TextBoxDiv').length==0){
 
+		//if($("#textbox").val()==null){
+
 			$('.newProjectCreateDiv').append(newTextBoxDiv);
 
-		}	
+			counter++;
+
+			$('#textbox').keydown(function() {
+
+				var message = $("#textbox").val();
+
+				if (event.keyCode == 13) {
+
+					if (message == "") {
+
+						alert("Enter Some Text In Textarea");
+
+					} else {
+
+						var li = $('<li/>').appendTo('#menu');
+
+						$('<a />').text(message).attr('href', message).appendTo(li);
+
+						$('#TextBoxDiv').remove( );
+						
+						//var sub_ul = $('<ul/>').appendTo(li);
+
+						//newTextBoxDiv.after().html(message);
+
+					}
+
+					return false;
+				}
+
+			});
+
+		}
 
 	});
 
